0ec0c77fd34edc977d8128876cc1585762c5f6dd
Sebastian <sebastian.pfaff@forbrf.lth.se>
2018-05-30 10:20:04 +0200

    ex catching?

--- tiftest.py
+++ tiftest.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 #file=sif('/home/sebastian/ch3/2018-02-28/calib1.sif')
 #file=sif('/home/sebastian/Documents/Data/paper1/20171030_refl_16.sif')
 #file=mptif16('C:/Users/Catalysis/Pictures/oxtest1.tif')
-file=mptif16('C:/Users/Catalysis/Desktop/Data/SOR/2018-05-24/sor_4.tif')
+#file=mptif16('C:/Users/Catalysis/Desktop/Data/SOR/2018-05-24/sor_4.tif')
+file=mptif16('E:\sorjonas/ox5.tif')
+
 print(file.numimgframes())
 print(file.getimgdata())
 print(file.getnumfiles())
@@ -14,9 +16,9 @@ print(file.getnumfiles())
 print(file.numimgframesperfile())
 print(file.whichfile(1500))
 
-imagedata=np.swapaxes(file.readimg(0,-1),0,2)
+#imagedata=np.swapaxes(file.readimg(0,-1),0,2)
 #imagedata=np.swapaxes(file.readimg(1400,1499),0,2)
-#imagedata=np.swapaxes(plifimg.readimgav(file,0,11000,50),0,2)
+imagedata=np.swapaxes(plifimg.readimgav(file,0,-1,10),0,2)
 # imagedataB=np.roll(np.swapaxes(plifimg.readimgav(fileB,1,500,1),0,2),-2,1)
 # plt.imshow(imagedata[0,:,:])
 # plt.show()
@@ -28,7 +30,11 @@ pg.image(imagedata, title="LU SOR Viewer")
 if __name__ == '__main__':
     import sys
     if sys.flags.interactive != 1 or not hasattr(QtCore, 'PYQT_VERSION'):
-        pg.QtGui.QApplication.exec_()
+        try:
+            pg.QtGui.QApplication.exec_()
+        except Exception:
+            sys.exc_clear()
+        
 
 #plt.imshow(imagedata[0,:,:])
 #plt.show()
