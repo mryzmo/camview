bad0032f4e23820c300a39f8230ce9fa7d747df1
Sebastian Pfaff <pfaff.sebastian@gmail.com>
2021-05-10 19:00:57 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'camview',         # How you named your package folder (MyLib)
   packages = ['camview'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Sebastian Pfaff',                   # Type in your name
@@ -16,13 +16,12 @@ setup(
           'pyqtgraph',
           'numpy',
           'matplotlib',
-          'pickle',
           'scanf',
           'imageio',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Researchers',      # Define that your audience are developers
+    'Intended Audience :: Science/Research',      # Define that your audience are developers
     'Topic :: Scientific/Engineering :: Image Processing',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
